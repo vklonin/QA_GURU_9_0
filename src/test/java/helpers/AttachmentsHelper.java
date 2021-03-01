2a8445a70d0d5c1374fef9255b252ca776955700
vladimir klonin <vklonin@gmail.com>
2021-03-01 22:36:13 +0400

    added allure attachments - video

--- src/test/java/helpers/AttachmentsHelper.java
+++ src/test/java/helpers/AttachmentsHelper.java
@@ -39,7 +39,7 @@ public class AttachmentsHelper {
     }
 
     public static String getVideoUrl() {
-        return "https://selenoid.autotest.cloud/video/" + getSessionId() + ".mp4";
+        return "https://selenoid.autotests.cloud/video/" + getSessionId() + ".mp4";
     }
 
     public static String getSessionId(){
